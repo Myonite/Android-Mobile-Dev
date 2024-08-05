fd4fe24c05d2729d602fa700aee3c2f81f0498ad
Jordi Van Rysselberghe <100159060+Myonite@users.noreply.github.com>
2024-08-05 16:29:26 +0200

    Seperation en generalisation,

--- app/src/main/java/com/example/mixingstat/data/repository/CocktailRepositoryImpl.kt
+++ app/src/main/java/com/example/mixingstat/data/repository/CocktailRepositoryImpl.kt
@@ -4,23 +4,29 @@ import com.example.mixingstat.data.dao.CocktailDao
 import com.example.mixingstat.data.models.Cocktail
 import com.example.mixingstat.data.network.CocktailApiService
 import com.example.mixingstat.data.response.CocktailsResponse
-import com.example.mixingstat.utils.safeApiCall
+import com.example.mixingstat.utils.ApiUtils
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
-import retrofit2.Response
-import java.io.IOException
 import javax.inject.Inject
 
 class CocktailRepositoryImpl @Inject constructor(
     private val dao: CocktailDao,
-    private val apiService: CocktailApiService
+    private val apiService: CocktailApiService,
+    private val dispatcher: CoroutineDispatcher = Dispatchers.IO,
+    private val apiUtils: ApiUtils = ApiUtils(dispatcher),
 ) : CocktailRepository {
 
 
-    private val dispatcher = Dispatchers.IO
     override suspend fun insert(cocktail: Cocktail) {
         withContext(dispatcher) {
             dao.insert(cocktail)
+        }
+    }
+
+    private suspend fun synchronizeDatabase(cocktails: List<Cocktail>) {
+        withContext(dispatcher) {
+            dao.insertAll(cocktails)
         }
     }
 
@@ -36,9 +42,8 @@ class CocktailRepositoryImpl @Inject constructor(
         }
     }
 
-
     override suspend fun getCocktail(id: String): Cocktail? {
-        return performRequest(
+        return apiUtils.performRequest(
             apiCall = { apiService.getCocktail(id) },
             dbCall = { CocktailsResponse(listOf(dao.getCocktailById(id))) },
             processResponse = { response -> synchronizeDatabase(response.drinks) }
@@ -46,136 +51,64 @@ class CocktailRepositoryImpl @Inject constructor(
     }
 
     override suspend fun getRandomCocktail(): Cocktail? {
-        return withContext(dispatcher) {
-            safeApiCall(
-                apiCall = {
-                    val response = apiService.getRandomCocktail()
-                    val cocktailsResponse = response.body()
-                    if (cocktailsResponse != null && cocktailsResponse.drinks.isNotEmpty()) {
-                        cocktailsResponse.drinks[0]
-                    } else {
-                        null
-                    }
-                },
-                dbCall = {      dao.getRandomCocktail()}
-            )
-        }
+        return apiUtils.performRequest(
+            apiCall = { apiService.getRandomCocktail() },
+            dbCall = { CocktailsResponse(listOf(dao.getRandomCocktail())) },
+            processResponse = { response -> synchronizeDatabase(response.drinks) }
+        ).drinks.firstOrNull()
     }
 
     override suspend fun getAllPopularCocktails(): List<Cocktail> {
-        return withContext(dispatcher) {
-            safeApiCall(
-                apiCall = {
-                    val response = apiService.getAllPopularCocktails()
-                    val cocktailsResponse = response.body()
-                    if (cocktailsResponse != null && cocktailsResponse.drinks.isNotEmpty()) {
-                        cocktailsResponse.drinks.forEach { it.isPopular = true }
-                        dao.insertAll(cocktailsResponse.drinks)
-                        cocktailsResponse.drinks
-                    } else {
-                        emptyList<Cocktail>()
-                    }
-                },
-                dbCall = { dao.getAllPopularCocktails() }
-            )
-        }
+        return apiUtils.performRequest(
+            apiCall = { apiService.getAllPopularCocktails() },
+            dbCall = { CocktailsResponse(dao.getAllPopularCocktails()) },
+            processResponse = {
+                it.drinks.forEach { it.isPopular = true }
+                synchronizeDatabase(it.drinks)
+            }
+        ).drinks
     }
 
     override suspend fun getAllLatestCocktails(): List<Cocktail> {
-        return withContext(dispatcher) {
-            safeApiCall(
-                apiCall = {
-                    val response = apiService.getAllLatestCocktails()
-                    val cocktailsResponse = response.body()
-                    if (cocktailsResponse != null && cocktailsResponse.drinks.isNotEmpty()) {
-                        cocktailsResponse.drinks.forEach { it.isLatest = true }
-                        dao.insertAll(cocktailsResponse.drinks)
-                        cocktailsResponse.drinks
-                    } else {
-                        emptyList<Cocktail>()
-                    }
-                },
-                dbCall = { dao.getAllLatestCocktails() }
-            )
-        }
+        return apiUtils.performRequest(
+            apiCall = { apiService.getAllLatestCocktails() },
+            dbCall = { CocktailsResponse(dao.getAllLatestCocktails()) },
+            processResponse = {
+                it.drinks.forEach { it.isLatest = true }
+                synchronizeDatabase(it.drinks)
+            }
+        ).drinks
     }
 
-    override suspend fun searchCocktailsByFirstLetter(value: Char): List<Cocktail>? {
-        return withContext(dispatcher) {
-            safeApiCall(
-                apiCall = {
-                    val response = apiService.searchCocktailsByFirstLetter(value)
-                    val cocktailsResponse = response.body()
-                    if (cocktailsResponse?.drinks?.isNotEmpty() == true) {
-                        dao.insertAll(cocktailsResponse.drinks)
-                        cocktailsResponse.drinks
-                    } else {
-                        emptyList<Cocktail>()
-                    }
-                },
-                dbCall = { dao.searchCocktailsByFirstLetter(value) }
-            )
-        }
+    override suspend fun searchCocktailsByFirstLetter(value: Char): List<Cocktail> {
+        return apiUtils.performRequest(
+            apiCall = { apiService.searchCocktailsByFirstLetter(value) },
+            dbCall = { CocktailsResponse(dao.searchCocktailsByFirstLetter(value)) },
+            processResponse = {
+                synchronizeDatabase(it.drinks)
+            }
+        ).drinks
     }
 
-    override suspend fun searchCocktailByName(value: String): List<Cocktail>? {
-        return withContext(dispatcher) {
-            safeApiCall(
-                apiCall = {
-                    val response = apiService.searchCocktailByName(value)
-                    val cocktailsResponse = response.body()
-                    if (cocktailsResponse?.drinks?.isNotEmpty() == true) {
-                        dao.insertAll(cocktailsResponse.drinks)
-                        cocktailsResponse.drinks
-                    } else {
-                        emptyList()
-                    }
-                },
-                dbCall = { dao.searchCocktailByName(value) }
-            )
-        }
+    override suspend fun searchCocktailByName(value: String): List<Cocktail> {
+        return apiUtils.performRequest(
+            apiCall = { apiService.searchCocktailByName(value) },
+            dbCall = { CocktailsResponse(dao.searchCocktailByName(value)) },
+            processResponse = {
+                synchronizeDatabase(it.drinks)
+            }
+        ).drinks
     }
 
     override suspend fun searchByIngredientName(value: String): List<Cocktail> {
-        return withContext(dispatcher) {
-            safeApiCall(
-                apiCall = {
-                    val response = apiService.searchByIngredientName(value)
-                    val cocktailsResponse = response.body()
-                    if (cocktailsResponse?.drinks?.isNotEmpty() == true) {
-                        dao.insertAll(cocktailsResponse.drinks)
-                        cocktailsResponse.drinks
-                    } else {
-                        emptyList()
-                    }
-                },
-                dbCall = { dao.searchByIngredientName(value) }
-            )
-        }
+        return apiUtils.performRequest(
+            apiCall = { apiService.searchByIngredientName(value) },
+            dbCall = { CocktailsResponse(dao.searchByIngredientName(value)) },
+            processResponse = {
+                synchronizeDatabase(it.drinks)
+            }
+        ).drinks
     }
 
-    private suspend fun synchronizeDatabase(cocktails: List<Cocktail>) {
-        withContext(dispatcher) {
-            dao.insertAll(cocktails)
-        }
-    }
-    private suspend fun <T> performRequest(
-        apiCall: suspend () -> Response<T>,
-        dbCall: suspend () -> T?,
-        processResponse: suspend (T) -> Unit
-    ): T {
-        return withContext(dispatcher) {
-            safeApiCall(
-                apiCall = {
-                    val response = apiCall.invoke()
-                    val responseBody = response.body()
-                    if (responseBody != null) {
-                        processResponse(responseBody)
-                    }
-                    responseBody ?: throw IOException("Response body is null")
-                },
-                dbCall = { dbCall.invoke() ?: throw IOException("Database call returned null") }
-            )
-        }
-    }
+
 }
--- app/src/main/java/com/example/mixingstat/utils/ApiUtils.kt
+++ app/src/main/java/com/example/mixingstat/utils/ApiUtils.kt
@@ -1,9 +1,13 @@
 package com.example.mixingstat.utils
 
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.withContext
+import retrofit2.Response
 import java.io.IOException
 
+class ApiUtils(private val dispatcher: CoroutineDispatcher) {
 
-    suspend fun <T> safeApiCall(
+    private suspend fun <T> safeApiCall(
         apiCall: suspend () -> T,
         dbCall: suspend () -> T
     ): T {
@@ -13,3 +17,24 @@ import java.io.IOException
             dbCall()
         }
     }
+
+    suspend fun <T> performRequest(
+        apiCall: suspend () -> Response<T>,
+        dbCall: suspend () -> T?,
+        processResponse: suspend (T) -> Unit
+    ): T {
+        return withContext(dispatcher) {
+            safeApiCall(
+                apiCall = {
+                    val response = apiCall.invoke()
+                    val responseBody = response.body()
+                    if (responseBody != null) {
+                        processResponse(responseBody)
+                    }
+                    responseBody ?: throw IOException("Response body is null")
+                },
+                dbCall = { dbCall.invoke() ?: throw IOException("Database call returned null") }
+            )
+        }
+    }
+}
