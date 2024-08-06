ce5ffce556b379ee1ac4c0d9520def459827587f
Jordi Van Rysselberghe <100159060+Myonite@users.noreply.github.com>
2024-08-06 20:45:05 +0200

    kDocs for HomeScreenViewModel and searchviewmodel

--- app/src/main/java/com/example/mixingstat/presentation/viewmodel/home/HomeScreenViewModel.kt
+++ app/src/main/java/com/example/mixingstat/presentation/viewmodel/home/HomeScreenViewModel.kt
@@ -11,6 +11,13 @@ import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
+/**
+ * ViewModel for the HomeScreen. This ViewModel is responsible for managing the state of the HomeScreen and loading cocktails from the repository.
+ *
+ * @property repository The repository from which cocktails are loaded.
+ * @property _state The mutable state flow that represents the state of the HomeScreen.
+ * @property state The state flow that represents the state of the HomeScreen. This is what UI components should observe.
+ */
 @HiltViewModel
 open class  HomeScreenViewModel @Inject constructor(
     private val repository: CocktailRepository
@@ -18,11 +25,18 @@ open class  HomeScreenViewModel @Inject constructor(
     private val _state = MutableStateFlow(HomeScreenState())
     open val state: StateFlow<HomeScreenState> = _state
 
+
+    /**
+     * Initializes the ViewModel by loading cocktails and observing the network status.
+     */
     init {
         loadCocktails()
         observeNetworkStatus()
     }
 
+    /**
+     * Observes the network status and loads cocktails when the network is connected.
+     */
     private fun observeNetworkStatus() {
         viewModelScope.launch {
             NetworkStatus.isConnected.collect { isConnected ->
@@ -33,6 +47,9 @@ open class  HomeScreenViewModel @Inject constructor(
         }
     }
 
+    /**
+     * Loads cocktails from the repository and updates the state.
+     */
     private fun loadCocktails() {
         viewModelScope.launch {
             _state.value = HomeScreenState(isLoading = true)
--- app/src/main/java/com/example/mixingstat/presentation/viewmodel/search/SearchViewModel.kt
+++ app/src/main/java/com/example/mixingstat/presentation/viewmodel/search/SearchViewModel.kt
@@ -13,7 +13,15 @@ import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
-
+/**
+ * ViewModel for the Search screen. This ViewModel is responsible for managing the state of the Search screen and performing search operations through the repository.
+ *
+ * @property repository The repository from which cocktails are searched.
+ * @property query The mutable live data that represents the search query.
+ * @property method The mutable live data that represents the search method.
+ * @property _searchResults The mutable state flow that represents the search results.
+ * @property searchResults The state flow that represents the search results. This is what UI components should observe.
+ */
 @HiltViewModel
 class SearchViewModel @Inject constructor(private val repository: CocktailRepository) :
     ViewModel() {
@@ -22,7 +30,12 @@ class SearchViewModel @Inject constructor(private val repository: CocktailReposi
     private val _searchResults = MutableStateFlow<List<Cocktail>>(emptyList())
     val searchResults: StateFlow<List<Cocktail>> = _searchResults
 
-
+    /**
+     * Performs a search operation based on the current query and method. The search operation is performed asynchronously in the ViewModel scope.
+     * The search method can be either "cocktail" or "ingredient". If it's "cocktail", the search is performed by the first letter if the query length is 1, otherwise by name.
+     * If the search method is "ingredient", the search is performed by ingredient name.
+     * The results of the search operation are stored in _searchResults.
+     */
     fun performSearch() {
         viewModelScope.launch {
             val formattedQuery = query.value?.replace(" ", "_") ?: ""
