8851e8bb68b45386167d2c3a6e41ea779ea3f452
Jordi Van Rysselberghe <100159060+Myonite@users.noreply.github.com>
2024-08-06 16:54:34 +0200

    Fixed the dependencies again, had way to much clutter

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,4 +7,5 @@ plugins {
     alias(libs.plugins.hilt) apply false
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.compose) apply false
+    id("com.autonomousapps.dependency-analysis") version "1.33.0"
 }
